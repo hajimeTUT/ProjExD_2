fb7da213263c5c1c58859dfb5e0d6f047515bf7c
hajime <c011918477@edu.teu.ac.jp>
2024-04-23 14:10:06 +0900

    移動量辞書の導入

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 800, 450
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -11,11 +11,19 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.transform.scale(bg_img,(WIDTH, HEIGHT))
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
+    kk_rct.center = WIDTH*9/16, HEIGHT*4/9
     clock = pg.time.Clock()
     tmr = 0
+    DELTA = { # 移動量辞書
+        pg.K_UP: (0, -5), 
+        pg.K_DOWN: (0, +5), 
+        pg.K_LEFT: (-5, 0), 
+        pg.K_RIGHT: (+5, 0)
+    }
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -24,6 +32,12 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
+        for k, v in DELTA.items():
+            if key_lst[k]:
+                sum_mv[0] += v[0]
+                sum_mv[1] += v[1]
+        """
         if key_lst[pg.K_UP]:
             sum_mv[1] -= 5
         if key_lst[pg.K_DOWN]:
@@ -32,6 +46,8 @@ def main():
             sum_mv[0] -= 5
         if key_lst[pg.K_RIGHT]:
             sum_mv[0] += 5
+        """
+
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
