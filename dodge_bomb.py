e2b43fcb669ba7eab41b1304bbd9defe0bef6cd7
hajime <c011918477@edu.teu.ac.jp>
2024-04-23 14:27:15 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -15,6 +16,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = WIDTH*9/16, HEIGHT*4/9
+    circle = pg.Surface((10,10))
+    bd_img = pg.Surface((20,20))
+    bd_img.set_colorkey((0,0,0))
+    pg.draw.circle(circle, (255,0,0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     clock = pg.time.Clock()
     tmr = 0
     DELTA = { # 移動量辞書
@@ -37,19 +44,12 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
-        """
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
-        """
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        bd_rct.move_ip(20, 20)
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
